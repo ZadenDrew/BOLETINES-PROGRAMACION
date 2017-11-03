c0485a86807674c389aa0390321e0a542fc33955
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2017-11-03 10:16:31 +0100

    exercicio2_10

---
+++ boletin2_10/src/boletin2_10/Boletin2_10.java
@@ -0,0 +1,21 @@
+package boletin2_10;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class Boletin2_10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       float soldo,comision,kilometraxe,dietas,sliquido,sbruto;
+       Scanner leer= new Scanner (System.in);
+         System.out.println("Introduce euros para cambio:");
+         soldo =leer.nextFloat();
+    }
+    
+}
