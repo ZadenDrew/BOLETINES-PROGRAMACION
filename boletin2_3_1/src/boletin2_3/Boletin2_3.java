4219047dbb7dd036bac98cb7aa7d18b03b406a02
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2017-11-03 10:27:46 +0100

    exercicio2_3_1

---
+++ boletin2_3_1/src/boletin2_3/Boletin2_3.java
@@ -0,0 +1,26 @@
+package boletin2_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class Boletin2_3 {
+//float cambio=1.1732f;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner leer= new Scanner (System.in);
+            float cambio,euros,dolares;
+            System.out.println("Introduce euros:");
+            euros=leer.nextFloat();
+            System.out.println("Introduce cambio:");
+            cambio = leer.nextFloat();
+            System.out.println(euros+"euros son"+euros*cambio+"dolares");
+            
+                    
+    }
+    
+}
