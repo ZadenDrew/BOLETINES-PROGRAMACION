5046ab8f41b0f7d556bd1daf6cff5b42606322b7
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2017-11-03 10:17:55 +0100

    exercicio2_2_1

---
+++ boletin2_2_1/src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,19 @@
+
+package boletin2_2;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class Boletin2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float lado=3f,area;
+        area= (float)Math.pow(lado, 2);
+        System.out.println("area"+lado*lado);
+    }
+    
+}
