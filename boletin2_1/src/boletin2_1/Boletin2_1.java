7b947cb8433b69732d1ad0134347156f40ec7ad3
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2017-11-03 10:14:46 +0100

    exercicio2_1

---
+++ boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,18 @@
+package boletin2_1;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float base =4f,altura=3f,area;
+        area=base*altura/2;
+        System.out.println("El área es:"+area);
+    }
+    
+}
